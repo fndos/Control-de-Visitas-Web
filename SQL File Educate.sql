eeee02aa699e0d9b22a3a3f23a3025bfe8efb604
fndos <fndos@hotmail.com>
2018-08-15 13:30:22 -0500

    SQL File Educate Updated

--- SQL File Educate.sql
+++ SQL File Educate.sql
@@ -2,20 +2,20 @@
 CREATE DATABASE educate;
 USE educate;
 
-INSERT INTO accounts_sector (id, name, is_active, created_by, date_joined) VALUES (1, 'A', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d')); 
-INSERT INTO accounts_sector (id, name, is_active, created_by, date_joined) VALUES (2, 'B', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
-INSERT INTO accounts_sector (id, name, is_active, created_by, date_joined) VALUES (3, 'C', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
-INSERT INTO accounts_sector (id, name, is_active, created_by, date_joined) VALUES (4, 'D', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
-INSERT INTO accounts_sector (id, name, is_active, created_by, date_joined) VALUES (5, 'E', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
+INSERT INTO sector (id, name, is_active, created_by, date_joined) VALUES (1, 'A', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d')); 
+INSERT INTO sector (id, name, is_active, created_by, date_joined) VALUES (2, 'B', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
+INSERT INTO sector (id, name, is_active, created_by, date_joined) VALUES (3, 'C', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
+INSERT INTO sector (id, name, is_active, created_by, date_joined) VALUES (4, 'D', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
+INSERT INTO sector (id, name, is_active, created_by, date_joined) VALUES (5, 'E', True, 'Alex Jordán', DATE_FORMAT(NOW(),'%Y-%m-%d'));
 
-INSERT INTO accounts_user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
+INSERT INTO user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
 VALUES (1, 'pbkdf2_sha256$100000$mEeRStxzv1wk$TG+/2P94ebK0JpUw6Lp/DlE/aNpWWiDX93vrSo4KmJw=', 1, 'tutor', 'Fernando', 'Sánchez', 'tutor@example.com', 1, 1, '2018-08-06 19:17:01.167838', '0929858736', '+593958477889', 1);
 
-INSERT INTO accounts_user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
+INSERT INTO user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
 VALUES (2, 'pbkdf2_sha256$100000$mEeRStxzv1wk$TG+/2P94ebK0JpUw6Lp/DlE/aNpWWiDX93vrSo4KmJw=', 1, 'tech', 'Jorge', 'Ayala', 'tech@example.com', 1, 1, '2018-08-06 19:17:01.167838', '1201333389', '+593958477889', 2);
 
-INSERT INTO accounts_user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
+INSERT INTO user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
 VALUES (3, 'pbkdf2_sha256$100000$mEeRStxzv1wk$TG+/2P94ebK0JpUw6Lp/DlE/aNpWWiDX93vrSo4KmJw=', 1, 'tutor_leader', 'Israel', 'Zurita', 'tutor_leader@example.com', 1, 1, '2018-08-06 19:17:01.167838', '1314344988', '+593958477889', 3);
 
-INSERT INTO accounts_user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
+INSERT INTO user (id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, dni, phone_number, user_type) 
 VALUES (4, 'pbkdf2_sha256$100000$mEeRStxzv1wk$TG+/2P94ebK0JpUw6Lp/DlE/aNpWWiDX93vrSo4KmJw=', 1, 'tech_leader', 'Alex', 'Jordán', 'tech_leader@example.com', 1, 1, '2018-08-06 19:17:01.167838', '3232848891', '+593958477889', 4);
